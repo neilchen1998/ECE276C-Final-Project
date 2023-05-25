cdb249baf34bc85cf3867d0b16aacb43441cd9e2
MaxGh <mghesqui@ucsd.edu>
2023-05-24 22:31:39 -0700

    Update projection.py
    made it stop at convergence (maxiter can be removed in future if needed)
    
    Also tested on left hand plane constraint, and plotted the qspace for the constraint, seemed consistent

--- projection.py
+++ projection.py
@@ -1,5 +1,6 @@
 import numpy as np
 import roboticstoolbox as rtb
+from matplotlib import pyplot as plt
 
 
 def project_to_constraint(q_in, constraint_grad, lims, robot):
@@ -15,27 +16,55 @@ def project_to_constraint(q_in, constraint_grad, lims, robot):
 	qtemp = q_in
 	for i in range(max_iter):
 		J = robot.jacob0(qtemp)
-		x = robot.fkine(np.reshape(qtemp,(2,)))
+		x = robot.fkine(np.reshape(qtemp, (2,)))
 		grad = constraint_grad(x)
-		qnext = qtemp-step_size*np.reshape(grad@J,(2,1))
+		step = step_size * np.reshape(grad @ J, (2, 1))
+		qnext = qtemp - step
 		qtemp = qnext
+		if np.linalg.norm(step) < 0.0001:
+			break
+	qtemp = np.reshape(qtemp, (2,))
 	return qtemp
 
 
 if __name__ == '__main__':
-	print('hello world')
-	q = np.zeros((2, 1))+1
+
+	q = np.random.normal(0,1,(2,1))
 
 
+	# drive to 0
+	# def constr(X):
+	#	x = X.t
+	#	grad = np.reshape(x,(1,3))
+	#	grad = np.hstack([grad,np.zeros((1,3))])
+	#	return grad
+
+	# drive to left hand plane
 	def constr(X):
 		x = X.t
-		grad = np.reshape(x,(1,3))
-		grad = np.hstack([grad,np.zeros((1,3))])
+		grad = np.zeros((1, 6))
+		if x[0] >= 0:
+			grad[0, 0] = x[0]
 		return grad
 
 
 	lim = [(-np.pi, np.pi), (-np.pi, np.pi)]
 	rob = rtb.models.DH.Planar2()
-	rob.plot([0,0])
-	qout = project_to_constraint(q, constr, lim, rob)
-	rob.plot(qout)
+	#rob.plot(np.reshape(q,(2,)))
+	#qout = project_to_constraint(q, constr, lim, rob)
+	#rob.plot(qout)
+
+	xs = np.array([0])
+	ys = np.array([0])
+	deg = np.pi/180
+	for i in range(90):
+		for j in range(90):
+			q = np.array([4*i*deg,4*j*deg])-np.pi
+			q = np.reshape(q,(2,1))
+			qout = project_to_constraint(q,constr,lim,rob)
+			xs = np.vstack([xs,qout[0]])
+			ys = np.vstack([ys, qout[1]])
+			print(i,',',j)
+
+	plt.scatter(xs,ys)
+	print('hello world')
