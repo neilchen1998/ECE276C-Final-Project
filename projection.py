42388fc3a394b9ffb72840564874cb5229bd52b3
MaxGh <mghesqui@ucsd.edu>
2023-05-25 12:41:17 -0700

    Update projection.py
    added a version which uses scipy optimize (somewhat faster on simple constraints, likely much faster on complicated constraints). For now can only handle a single constraint function, may be worth expanding in future

--- projection.py
+++ projection.py
@@ -1,6 +1,8 @@
 import numpy as np
 import roboticstoolbox as rtb
 from matplotlib import pyplot as plt
+import scipy.optimize as opti
+import time
 
 
 def project_to_constraint(q_in, constraint_grad, lims, robot):
@@ -26,6 +28,24 @@ def project_to_constraint(q_in, constraint_grad, lims, robot):
 	qtemp = np.reshape(qtemp, (2,))
 	return qtemp
 
+def project_to_constraint_scipy(q_in, constraint,consttype, lims, robot):
+	"""
+	:param q_in: q-space point to be projected, as (2,)
+	:param constraint: constraint function on c-space
+	:param consttype: 'eq' for equality, 'ineq' for inequality
+	:param lims: joint limits as list of tuples
+	:param robot: robot for fkine
+	:return: the closest point on the constrained space to the given point
+	"""
+	def loss(x):
+		return np.linalg.norm(np.reshape(q_in,x.shape)-x)
+	def my_constraint(q):
+		x = robot.fkine(np.reshape(q,(2,)))
+		return constraint(x)
+	consts = [{'type':consttype,'fun':my_constraint},]
+	qout = opti.minimize(loss,np.reshape(q_in,(2,)),method='slsqp',bounds=lims,constraints=consts,options={'disp':False, 'ftol':1e-6, 'maxiter':2000})
+	return qout.x
+
 
 if __name__ == '__main__':
 
@@ -47,24 +67,27 @@ if __name__ == '__main__':
 			grad[0, 0] = x[0]
 		return grad
 
+	def constr2(X):
+		x = X.t
+		return -x[0]
 
 	lim = [(-np.pi, np.pi), (-np.pi, np.pi)]
 	rob = rtb.models.DH.Planar2()
-	#rob.plot(np.reshape(q,(2,)))
+	rob.plot(np.reshape(q,(2,)))
 	#qout = project_to_constraint(q, constr, lim, rob)
+	#qout = project_to_constraint_scipy(q,constr2,'ineq',lim,rob)
 	#rob.plot(qout)
 
-	xs = np.array([0])
-	ys = np.array([0])
+	fig = plt.figure()
 	deg = np.pi/180
+	start_t = time.time()
 	for i in range(90):
 		for j in range(90):
 			q = np.array([4*i*deg,4*j*deg])-np.pi
 			q = np.reshape(q,(2,1))
 			qout = project_to_constraint(q,constr,lim,rob)
-			xs = np.vstack([xs,qout[0]])
-			ys = np.vstack([ys, qout[1]])
+	#		qout = project_to_constraint_scipy(q, constr2, 'ineq', lim, rob)
 			print(i,',',j)
-
-	plt.scatter(xs,ys)
+	print(start_t)
+	print(time.time())
 	print('hello world')
