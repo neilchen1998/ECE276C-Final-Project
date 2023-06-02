658e26ace262917f4e23562b9cdad700ad0226a3
Neil Chen <neilchen1998@gmail.com>
2023-06-02 10:27:15 -0700

    made the graph & the obstacle true to scale

--- visualization.py
+++ visualization.py
@@ -4,16 +4,21 @@ import numpy as np
 if __name__ == "__main__":
 
     data = np.load('path-PRMStar.npy', allow_pickle=True)
-    radius = 0.25
+
+    plt.figure(figsize=[5, 5])
+    ax = plt.axes([0.1, 0.1, 0.8, 0.8], xlim=(0, 1), ylim=(0, 1))
     points_whole_ax = 5 * 0.8 * 72    # 1 point = dpi / 72 pixels
+    radius = 0.25
     points_radius = 2 * radius / 1.0 * points_whole_ax
-    plt.scatter(.5, .5, s=points_radius**2, c='gray')
+    ax.scatter(0.5, 0.5, s=points_radius**2, color='gray')
+
     for i, point in enumerate(data):
-        plt.scatter(point[0], point[1])
+        ax.scatter(point[0], point[1])
         if i > 0:
             x_values = [data[i-1][0], data[i][0]]
             y_values = [data[i-1][1], data[i][1]]
             plt.plot(x_values, y_values, linestyle="--")
 
     plt.title('PRMStar')
+    plt.grid()
     plt.show()
