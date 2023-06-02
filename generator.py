7293ed7c1ad7371f371ed612e703d0188c758d12
MaxGh <mghesqui@ucsd.edu>
2023-06-01 17:29:45 -0700

    Create generator.py
    created an api for the generator abstract base class

---
+++ generator.py
@@ -0,0 +1,36 @@
+#import stuff
+from abc import ABC, abstractmethod
+
+class Generator(ABC):
+    """
+    Abstract base class for generators
+    """
+    @abstractmethod
+    def generate(self,seed):
+        '''
+        takes in a random seed and returns a sample
+        '''
+        pass
+
+
+class Baseline_Generator(Generator):
+    """
+    Generator which just projects samples to constrained space
+    """
+    def __init__(self,constraints,constr_types,joint_lims,robot):
+        '''
+        constraints: a list of constraint functions that take a cartesian space point as input
+        constr_types: list of types 'eq' for equality, 'ineq' for inequality
+        joint_lims: the joint limits of the robot as a list of tuples [(min,max),...]
+        robot: the robot used for fkine
+        '''
+        self.robot = robot
+        self.constraints = constraints
+        self.constr_types = constr_types
+        self.j_lims = joint_lims
+    
+    def generate(self,seed):
+        '''
+        takes in a random seed and returns a sample
+        '''
+        pass
